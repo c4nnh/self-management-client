7e536935ea0ed39864a3e7a526681f48aa6a01ad
c4nnh <cannh.99@gmail.com>
2022-12-08 19:35:54 +0700

    add local font

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import { Button } from "antd";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { useTranslation } from "react-i18next";
 
 function App() {
-  const [count, setCount] = useState(0);
   const { t } = useTranslation();
 
   return (
@@ -15,24 +12,10 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src="/logo.svg" className="logo" alt="Vite logo" />
         </a>
-        <span>{t("abcd.ef")}</span>
       </div>
       <StyledDiv>
-        <span>hello</span>
+        <span className="font-thin text-3xl">hello it is me</span>
       </StyledDiv>
-      <h1 className="text-red-200">Vite + React</h1>
-      <Button type="primary">asd</Button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
